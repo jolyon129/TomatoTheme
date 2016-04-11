112c8df0770c3a6435bf9052484f38eb3b14fb72
左伦 <jarodlee.lzl@alibaba-inc.com>
2016-04-11 19:53:21 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var exec = require('child_process').exec;
+
+// gulp.task('minify', function () {
+//   gulp.src('js/app.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('build'))
+// });
+
+gulp.task('watch', function() {
+    gulp.watch('layout/*.swig', function() {
+        console.log('I HEAR YOU');
+    });
+});
+
+gulp.task('hexo', function() {
+    exec('cd ../../ && hexo s');
+});
+
+
